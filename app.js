71e2232192f048d42d1983242023f47622dd4f15
faly9 <randriantsoafaly8@gmail.com>
2025-06-14 11:03:51 +0300

    feat(back): add controllers

---
+++ app.js
@@ -0,0 +1,29 @@
+// app.js
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const connection = require('express-myconnection');
+const mysql = require('mysql');
+
+const routes = require('./routes');
+const dbConfig = require('./config/database');
+const { middleware } = require('./middlewares/headerAnalysis');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(connection(mysql, dbConfig, 'pool'));
+
+app.set('view engine', 'ejs');
+app.set('views', 'composants');
+
+// Middleware pour analyser les headers
+app.use(middleware);
+
+// Routes
+app.use('/', routes);
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Serveur écoute sur http://localhost:${port}`);
+});
