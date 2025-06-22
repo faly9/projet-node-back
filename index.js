968a992edeea58d2db5ce512882f89b7bd92bcb0
flavienrak <flavien.andrisoarak@gmail.com>
2025-06-22 22:19:58 +0300

    feat(todo): add crud todo

--- index.js
+++ index.js
@@ -3,18 +3,32 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const connection = require('express-myconnection');
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const dotenv = require('dotenv');
+const cors = require('cors');
 
 dotenv.config();
 
+const frontendUri = process.env.FRONTEND_URI;
 
-const routes = require('./routes');
+const todoRoutes = require('./routes/todo');
 const dbConfig = require('./config/database');
+
 const { middleware } = require('./middlewares/headerAnalysis');
+const { explainHeaders } = require('./middlewares/header.middleware');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(
+  cors({
+    origin: [frontendUri],
+    credentials: true,
+    preflightContinue: false,
+    allowedHeaders: ['sessionId', 'Content-Type'],
+    exposedHeaders: ['sessionId'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  })
+);
 
 app.use(connection(mysql, dbConfig, 'pool'));
 
@@ -23,9 +37,10 @@ app.set('views', 'composants');
 
 // Middleware pour analyser les headers
 app.use(middleware);
+app.use(explainHeaders);
 
-// Routes
-app.use('/', routes);
+// TODO ROUTES
+app.use('/api/todo', todoRoutes);
 
 const port = process.env.PORT;
 app.listen(port, () => {
